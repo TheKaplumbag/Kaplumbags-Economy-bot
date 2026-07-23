5c776d711c98e155386cd7d422e7def6bddb7dae
TheKaplumbag <scpmtf4@gmail.com>
2026-07-23 19:34:38 +0300

    Quick commit: Update Cogs/Economy.py

--- Cogs/Economy.py
+++ Cogs/Economy.py
@@ -13,7 +13,7 @@ class EconomyCommands(commands.Cog):
   def __init__(self, bot: commands.Bot):
     self.bot = bot
     
-  Owner_Group = app_commands.Group(name="owner", description="Owner only")
+  Economy_Group = app_commands.Group(name="economy", description="Economy commands")
 
   
 
@@ -21,20 +21,6 @@ class EconomyCommands(commands.Cog):
   async def wallet(self, interaction: discord.Interaction, user: discord.User):
     coins = getCoins(user.id)
     await interaction.response.send_message(f"@{user} has {coins}¢ in their wallet! ")
-
-
-  @Owner_Group.command(name="give-money", description="gives money no matter what")
-  @is_owner()
-  async def giveMoney(self, interaction: discord.Interaction, who: discord.User, amount: app_commands.Range[int, 1, 9999]):
-    status, msg = addMoney(who.id, amount)
-    if status == True:
-      await interaction.response.send_message(f"{amount}¢ money has been given to @{who}")
-    else:
-      print(msg)
-
-  async def giveMoney_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
-    if isinstance(error, app_commands.CheckFailure):
-      await interaction.response.send_message("❌ Only the bot owner can use this command!", ephemeral=True)
 
 
   @app_commands.command(name="daily", description="daily free cash")
